22ed8a08a6f570fe487dbf4c66cd36e8a2457e69
torokv@vnet.hu <Kapitany>
2023-04-24 20:44:08 +0200

    Adding LocalFunctions example

--- Program.cs
+++ Program.cs
@@ -18,12 +18,34 @@
         private static void HexaAndBinaryLiterals()
         {
             Console.WriteLine("Hexadecimal and binary literals");
-            
+
             int hexa = 0xff;
             Console.WriteLine(hexa);
 
             int bin = 0b1111_1111;
             Console.WriteLine(bin);
+
+            Console.WriteLine();
+        }
+
+        private static void LocalFunctions()
+        {
+            Func<double, double> SquareLambda = x => x * x;
+
+            double Square(double x)
+            {
+                return x * x;
+            }
+
+            double Cube(double x) => x * x * x;
+
+            Console.WriteLine("Local functions");
+
+            double number = 5.0;
+            Console.WriteLine($"The square of {number} = {SquareLambda(number)}");
+            Console.WriteLine($"The square of {number} = {Square(number)}");
+            Console.WriteLine($"The cube of {number}   = {Cube(number)}");
+            Console.WriteLine();
         }
 
         static void Main(string[] args)
@@ -34,6 +56,7 @@
 
             ReadableNumericLiterals();
             HexaAndBinaryLiterals();
+            LocalFunctions();
         }
     }
 }
